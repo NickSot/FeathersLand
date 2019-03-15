78a41680924ddec48ef6ebfccc6377ab84420a40
Mirrorlock <mar.penev@gmail.com>
2019-03-15 11:46:12 +0200

    Delete node modules

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser')
 var expressLayout = require("express-ejs-layouts");
+var db = require("./config/database");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- config/database.js
+++ config/database.js
@@ -4,8 +4,14 @@ const fs    = require('fs');
 var db = mysql.createConnection({
   "host": "localhost",
   "user": "root",
-  "password": fs.readFileSync('password.json').password,
+  "password": fs.readFileSync('./password.json').password,
   "database": "WritersDenDB"
+}); 
+
+
+db.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to mysql database");
 });
 
 module.exports = db;
