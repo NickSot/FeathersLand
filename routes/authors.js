bf931ab7f9e50b504374af4820f191a35738d5f0
NickSot <nikola.s.sotirov@gmail.com>
2019-03-16 00:13:44 +0200

    There was a need for change in the authors.js

--- routes/authors.js
+++ routes/authors.js
@@ -5,9 +5,16 @@ var db = require('../config/database');
 router.get('/', function(req, res, next) {
     res.locals.authenticated = req.session.authenticated;
 
-    db.query('Select * From Users', (err, result) => {
-        res.render('authors', {users: result});
-    });
+    if (req.session.user != null && req.session.user != undefined){
+        db.query('Select * From Users Where ID <> ?', [req.session.user.ID], (err, result) => {
+            res.render('authors', {users: result});
+        });
+    }
+    else{
+        db.query('Select * From Users Where ID', (err, result) => {
+            res.render('authors', {users: result});
+        });
+    }
 });
 
 module.exports = router;
