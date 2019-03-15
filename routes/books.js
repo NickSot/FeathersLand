4393343b020c6a001f33531413f72f7ff6c494e3
Mirrorlock <mar.penev@gmail.com>
2019-03-16 00:43:19 +0200

    All books access created. Pages implemented

--- routes/books.js
+++ routes/books.js
@@ -4,20 +4,20 @@ var db = require("../config/database");
 
 const booksOnPage = 30;
 
-router.get('/:page', function(req, res, next) {
-    let offset = (req.params.page - 1) * 10;
+router.get('/', function(req, res, next) {
+    let offset = (req.query.page - 1) * 10;
     let query = 'select * from Books order by Rating desc LIMIT ' + booksOnPage + ' OFFSET ' +  offset;
-    // console.log("Query is:" + query);
+
     db.query(query, (err, result) => {
-        console.log("Offset is:", offset);
         if(err) throw err;
         if(result.length > 0){
 
             res.locals.authenticated = req.session.authenticated;
-            res.render('books', {books : result});
+            // console.log("In the end: " + Math.ceil(result.length / booksOnPage))
+            res.render('books', {books : result, 'numPages' : Math.ceil(result.length / booksOnPage)});
 
         }else{
-            req.flash('error', "Изглежда сме в непродуктивен период...");
+            req.flash('error', "Изглежда свършиха книгите...");
             res.redirect('/');
         }
     });
