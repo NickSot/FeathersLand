c527e577a75b912c8579eb4016bf4befb3b15a72
NickSot <nikola.s.sotirov@gmail.com>
2019-03-16 01:34:46 +0200

    Started working on user relations

--- routes/author.js
+++ routes/author.js
@@ -17,4 +17,34 @@ router.get('/:id/show', function(req, res, next) {
     });
 });
 
+router.post('/:id/show', (req, res) => {
+    if (!req.session.authenticated){
+        req.flash('info', `Трябва да си влязъл в акаунта си, за да достъпиш тази опция!`);
+        req.session.authenticated = false;
+        res.locals.authenticated = req.session.authenticated;
+        res.render('index');
+    }
+    else{
+        res.locals.authenticated = req.session.authenticated;
+
+        let user = req.body.user;
+
+        console.log("User: " + user);
+        
+        db.query('Select * From Followers Where FollowingId = ?', [user.ID], (err, result) => {
+            if (result.length != 0){
+                req.flash('info', 'Вече си абониран за този автор!');
+                res.redirect('/author/' + user.ID + '/show');
+            }
+            else{
+                req.flash('info', 'Успешно абониране за автора!');
+                db.query('Insert Into Followers (FollowerId, FollowingId) Values (?, ?)', [req.session.user.ID, user.ID], (err, result) => {
+                    req.flash('info', 'Успешно абониране!');
+                    res.redirect('/authors');
+                });
+            }
+        });
+    }
+});
+
 module.exports = router;
