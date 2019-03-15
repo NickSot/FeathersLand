0f2e9357ddef13cbbca8f43c65c81ba4778d2380
Dimitar Stoilov <dks@aeneas.skyaffie.com>
2019-03-15 17:23:04 +0200

    updated layout

--- routes/write.js
+++ routes/write.js
@@ -2,13 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    if (req.session.authenticated){
-        res.render('write');
-    }
-    else{
-        req.flash('info', 'Трябва да си влязъл/ла в профила си, за да достъпиш тази опция!');
-        res.render('index');
-    }
+    res.render('write',{layout:false});
 });
 
 module.exports = router;
