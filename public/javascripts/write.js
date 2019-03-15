950aad0bddb915f58ab5af5f26174a3859a30d83
titi_2001 <titi_2001@abv.bg>
2019-03-15 11:07:08 +0200

    writing beta

---
+++ public/javascripts/write.js
@@ -0,0 +1,4 @@
+<script>
+setInterval(function update(){
+  document.getElementById("writingResult").innerHTML = '<p>' + document.getElementById("writingInProgress").innerHTML + '</p>';
+}, 100);</script>
