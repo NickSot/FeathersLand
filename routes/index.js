953f7fa9eeaac2f5dbbc13172a94871336f333a1
NickSot <nikola.s.sotirov@gmail.com>
2019-03-15 12:23:00 +0200

    Just added register basic functionality

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,8 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', (req, res) => {
+  console.log('HERE');
+
   let rusername = req.body.runame;
 
   let rpwd = req.body.rpsw;
@@ -16,11 +18,37 @@ router.post('/', (req, res) => {
 
   let lpwd = req.body.lpsw;
 
+  let rreppsw = req.body.rreppsw;
+
   if (rusername == undefined && rpwd == undefined){
     //Login part
   }
   else{
-    //Register part
+    if (rreppsw != rpwd){
+      res.render('index');
+    }
+    else{
+      console.log('HERE');
+
+      db.query('Select * From Users Where username = ?', [rusername], (err, result) => {
+        if (err) throw err;
+
+        console.log(result);
+
+        if (result.length){
+          res.render('index');
+        }
+        else{
+          db.query('Insert Into Users (username, pass) Values (?, ?)', [rusername, rpwd], (err, result) => {
+            if (err) throw err;
+      
+            console.log(result);
+
+            res.render('index');
+          });
+        }
+      });
+    }
   }
 
   db.query('Select * From Books', (err, result) => {
