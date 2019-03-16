7c4a88b41b2702ab135cc594e6c456b97c7fb315
titi_2001 <titi_2001@abv.bg>
2019-03-16 16:34:50 +0200

    color change of button

--- public/javascripts/author.js
+++ public/javascripts/author.js
@@ -1,11 +1,11 @@
 function changeSubscribedStatus(){
     var elem = document.getElementById("subscribeButton");
-    if(elem.style.backgroundColor == "green"){
+    if(elem.style.backgroundColor == "#85ff60"){
       elem.style.innerHTML = "Абонирай се!";
       elem.style.backgroundColor = "#5F9BE0";
     }
     else {
-      elem.style.innerHTML = "!";
-      elem.style.backgroundColor = "green";
+      elem.style.innerHTML = "Абониран!";
+      elem.style.backgroundColor = "#85ff60";
     }
 }
