4750cc2cb08f1ab4b9d59e322a4256c45d08c764
Mirrorlock <mar.penev@gmail.com>
2019-03-16 10:58:07 +0200

    Added write controller with pandoc

--- routes/write.js
+++ routes/write.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var pandoc = require('node-pandoc');
 
-router.get('/', function(req, res, next) {
-    res.render('write',{layout:false});
+function generateThroughMarkdown(content){
+    var thing;
+    pandoc(content, '-f markdown -t html5', (err, result) =>{
+        if(err) throw err;
+        console.log(result);    
+        thing = result;
+    });
+    return thing;
+}
+
+router.get('/', (req, res) => {
+    res.locals.authenticated = req.session.authenticated;
+
+    res.render('write',{layout:false, markdown: generateThroughMarkdown});
+});
+
+router.post('/', (req, res) => {
+    let content = req.body.content;
+    console.log(content);
 });
 
 module.exports = router;
