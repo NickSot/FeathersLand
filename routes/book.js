1f9d132ac1115910e54db21e8a12e9a527570de6
NickSot <nikola.s.sotirov@gmail.com>
2019-03-16 10:53:49 +0200

    Books started

--- routes/book.js
+++ routes/book.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../config/database');
 
-router.get('/', function(req, res, next) {
+router.get('/:id', function(req, res, next) {
     res.locals.authenticated = req.session.authenticated;
+
+    db.query('Select * ');
+
     res.render('book');
 });
 
