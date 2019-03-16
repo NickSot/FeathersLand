9a1da8156ac8d8df1a8f5d8289d7f787486b04be
NickSot <nikola.s.sotirov@gmail.com>
2019-03-16 23:28:03 +0200

    Writing in chapter content is done!

--- routes/write.js
+++ routes/write.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var pandoc = require('node-pandoc');
 var db = require('../config/database');
 
-
 router.get('/mybook', (req, res) => {
     let bookId = req.query.bookId;
     let query = `SELECT ch.Title AS characterTitle, b.Title AS bookTitle
@@ -33,15 +32,30 @@ router.get('/', (req, res) => {
         req.flash('error', 'Трябва да си в акаунта си за да можеш да твориш');
         res.redirect("/");
     }
+    else{
+        res.locals.authenticated = session.authenticated;
+        
+        let query = "Select * from Books where AuthorId = ?";
+        
+        db.query(query, [session.user.ID], (err, userBooks) => {
+            if(err) throw err;
+            console.log(userBooks);
+            res.render('chooseBook', {books : userBooks });
+        });
+    }
+});
 
-    res.locals.authenticated = session.authenticated;
-    let query = "Select * from Books where AuthorId = ?";
-    db.query(query, [session.user.ID], (err, userBooks) => {
-        if(err) throw err;
-        console.log(userBooks);
-        res.render('chooseBook', {books : userBooks });
-    });
+var chapterId;
+var chapter;
 
+router.get('/:chapterId', (req, res) => {
+    chapterId = req.params.chapterId;
+
+    db.query('Select * From Chapters Where Id = ?', [chapterId], (err, chapters) => {
+        chapter = chapters[0];
+
+        res.render('write', {layout: false, chapter: chapter, result: null});
+    })
 });
 
 router.post('/', (req, res) => {
@@ -49,12 +63,20 @@ router.post('/', (req, res) => {
 
     let markdown = req.body.markdown;
 
-    
-
     pandoc(markdown, '-f markdown -t html5', (err, result) => {
         if (err) throw err;
 
-        res.json({'result': result});
+        db.query('Update Chapters Set Content = ? Where Id = ?', [markdown, chapterId], (err) => {
+            if (err){
+                throw err;
+            }
+           
+            console.log(chapter);
+
+            console.log('Updated!');
+
+            res.json({'result': result, chapter: chapter});
+        });
     });
 });
 
