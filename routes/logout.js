7b2bcfd13e887a35fa32dd130909004ba739df06
NickSot <nikola.s.sotirov@gmail.com>
2019-03-17 01:51:28 +0200

    Set logout to redirect to index

--- routes/logout.js
+++ routes/logout.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
     console.log(req.session);
     req.flash('info', 'Успешно отписване!');
 
-    res.redirect('index');
+    res.redirect('/');
 });
 
 module.exports = router;
