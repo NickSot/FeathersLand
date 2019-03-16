7dde597770e281b461c6f79a7f64ed5c20bfb2c2
Mirrorlock <mar.penev@gmail.com>
2019-03-16 08:20:18 +0200

    Books page now working

--- routes/books.js
+++ routes/books.js
@@ -2,19 +2,25 @@ var express = require('express');
 var router = express.Router();
 var db = require("../config/database");
 
-const booksOnPage = 30;
+const booksOnPage = 3;
 
 router.get('/', function(req, res, next) {
-    let offset = (req.query.page - 1) * 10;
-    let query = 'select * from Books order by Rating desc LIMIT ' + booksOnPage + ' OFFSET ' +  offset;
+    let offset = (req.query.page - 1) * booksOnPage;
+    let selectQuery = 'select * from Books order by Rating desc LIMIT ' + booksOnPage + ' OFFSET ' +  offset;
+    let countQuery = 'select count(*) from Books';
+    let booksCount;
+    db.query(countQuery, (err, result) => {
+        if(err) throw err;
+        booksCount = result[0]['count(*)'];
+    });
 
-    db.query(query, (err, result) => {
+    db.query(selectQuery, (err, result) => {
         if(err) throw err;
         if(result.length > 0){
-
+            console.log("result: " + result.length);
             res.locals.authenticated = req.session.authenticated;
-            // console.log("In the end: " + Math.ceil(result.length / booksOnPage))
-            res.render('books', {books : result, 'numPages' : Math.ceil(result.length / booksOnPage)});
+            console.log("In the end: " + Math.ceil(booksCount / booksOnPage))
+            res.render('books', {books : result, 'numPages' : Math.ceil(booksCount / booksOnPage)});
 
         }else{
             req.flash('error', "Изглежда свършиха книгите...");
